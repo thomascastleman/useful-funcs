3b503dd10b67a6426756c3f3c375322e2e0c6307
Thomas Castleman <thomas7rex@gmail.com>
2019-06-03 17:31:21 -0400

    Split random() into rand() and randSubset()

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 
-/*	Get a specified number of random elements from a given array, without repeats.
-	Returns an array of */
-exports.random = function(arr, num) {
-	// if non-empty array
+/* Get a single random element from an array */
+exports.rand = function(arr) {
 	if (arr.length > 0) {
-		// if a non-singleton random subset of arr requested
-		if (num) {
-			// if subset is strictly smaller than array
-			if (num < arr.length) {
-				// get all possible indices in this array
-				var indices = Array.apply(null, { length: arr.length }).map(Number.call, Number);
-				var subset = [];
-
-				for (var i = 0; i < num; i++) {
-					// get random index of an index in arr
-					var rand = Math.floor(Math.random() * indices.length);
-
-					// add actual element of arr to subset
-					subset.push(arr[indices[rand]]);
-
-					// remove index of element from indices list
-					indices.splice(rand, 1);
-				}
-
-				return subset;
-			} else {
-				return null;
-			}
-		} else {
-			// return a single random element
-			return arr[Math.floor(Math.random() * arr.length)];
-		}
+		// return a single random element
+		return arr[Math.floor(Math.random() * arr.length)];
 	} else {
-		// return null on empty array
 		return null;
+	}
+}
+
+/*	Get a specified number of random elements from a given array, without repeats.
+	Returns an empty array if given array is empty, or if more elements are requested than
+	exist in the given array. */
+exports.randSubset = function(arr, num) {
+	// if non-empty array and subset strictly smaller
+	if (arr.length > 0 && num < arr.length) {
+		// get all possible indices in this array
+		var indices = Array.apply(null, { length: arr.length }).map(Number.call, Number);
+		var subset = [];
+
+		for (var i = 0; i < num; i++) {
+			// get random index of an index in arr
+			var rand = Math.floor(Math.random() * indices.length);
+
+			// add actual element of arr to subset
+			subset.push(arr[indices[rand]]);
+
+			// remove index of element from indices list
+			indices.splice(rand, 1);
+		}
+
+		return subset;
+	} else {
+		// return empty subset
+		return [];
 	}
 }
 
@@ -56,5 +56,3 @@ exports.remove = function(arr, qual) {
 var a = ['test', 'another thing', 'one more', 'more data', 'eek'];
 
 var uf = exports;
-
-console.log(uf.random(a, 3));
